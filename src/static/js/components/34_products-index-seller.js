8ce7c3349a180f907be73f0fb8ca27d5c52cf394
L-Sebastian <lsebastian.pulgarin@gmail.com>
2026-02-25 18:04:50 -0500

    Dios provera, amen, jesus, todos los santos, la santisima trinidad, los dioses griegos, Diomedes días, ameeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeen

--- src/static/js/components/34_products-index-seller.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".products-index-seller-container");
-  
-  if (container) {
-    fetch("/src/templates/components/34_products-index-seller.html")
-      .then(res => res.text())
-      .then(data => container.innerHTML = data);
-  }
-});
